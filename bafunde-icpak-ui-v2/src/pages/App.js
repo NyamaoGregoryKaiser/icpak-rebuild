e30d6a87c3ac7915cd31b564d6798a23bbe1b7e6
NyamaoGregoryKaiser <nyamaokaiser79@gmail.com>
2025-04-07 00:17:32 +0300

    Add theme context to fix build

--- bafunde-icpak-ui-v2/src/pages/App.js
+++ bafunde-icpak-ui-v2/src/pages/App.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import '../app/globals.css';
 
 import { ThemeProvider } from '../components/theme-context'; // Import the ThemeProvider
-import App from './App';
+import { App } from './App';
 
-ReactDOM.render(
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <ThemeProvider>
     <App />
   </ThemeProvider>,
